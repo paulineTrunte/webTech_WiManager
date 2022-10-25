812d5fd34498ef271d700282cfae3fc8e98ac0b7
paulineTrunte <pauline.trunte@student.htw-berlin.de>
2022-10-25 15:27:35 +0200

    initial commit

---
+++ src/main/java/de/htwberlin/webtech/wiManager/WiManagerApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.wiManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/wiManager/WiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.wiManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
