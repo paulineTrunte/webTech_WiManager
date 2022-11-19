24e6a205ba4501dc817e314191510289af7a73a7
StevanPaunovic <stevan.paunovic@gmx.de>
2022-11-19 06:39:58 +0100

    Student neue Variablen gefixt

--- src/main/java/de/htwberlin/webtech/wiManager/web/api/Student.java
+++ src/main/java/de/htwberlin/webtech/wiManager/web/api/Student.java
@@ -6,12 +6,19 @@ public class Student {
     private String vorname;
     private String name;
 
+    private String email;
+
+    private int matrikelNr;
+
     public Student(long id, String vorname, String name, String email, int matrikelNr) {
         this.id = id;
         this.vorname = vorname;
         this.name = name;
+        this.email = email;
+        this.matrikelNr = matrikelNr;
     }
-    public Student(){
+
+    public Student() {
 
     }
 
@@ -37,5 +44,21 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMatrikelNr() {
+        return matrikelNr;
+    }
+
+    public void setMatrikelNr(int matrikelNr) {
+        this.matrikelNr = matrikelNr;
     }
 }
